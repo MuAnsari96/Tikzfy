7df6866d74a7fbb6609322f40ab3714b6308e982
Mustafa Ansari <maa296@cornell.edu>
2016-07-17 00:42:20 -0400

    Remove unecessary deps, set up for heroku

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var server = require('http').createServer(app);
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,19 +17,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // error handlers
 
@@ -57,4 +47,3 @@ app.use(function(err, req, res, next) {
 });
 
 server.listen(process.env.PORT || 8080);
-module.exports = app;
